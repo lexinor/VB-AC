fcba7c635e9b5066455dd54bddfd2bd2824c0f2c
visibait <33099726+visibait@users.noreply.github.com>
2021-04-24 14:27:04 +0200

    Delete authorization.lua

--- authorization.lua
+++
@@ -1,6 +0,0 @@
--- VISI AC PUBLIC VERSION (PROTECTED)
--- No tocar esto, ya que os romperá el Anticheat.
-
-Authorization_Token = "K9LGFG-ZGSWVM-7504BI-QU1F6C"
-authorization_1 = "yLiAgWbrfS-XkSZeTrl4E" 
-authorization_2 = "thTLrsmWQc-aCYugvInkD"
