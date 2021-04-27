981d0784f5afa8b5e162ab42c4c146fd8a3e3289
visibait <33099726+visibait@users.noreply.github.com>
2021-04-27 18:38:46 +0200

    VB-AC 2.0 Release

---
+++ VB-AC/fxmanifest.lua
@@ -0,0 +1,28 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+ui_page "html/index.html"
+
+client_scripts {
+    '@menuv/menuv.lua',
+    'configs/client_config.lua',
+    'client/*.lua'
+}
+
+server_scripts {
+    '@mysql-async/lib/MySQL.lua',
+    'configs/server_config.lua',
+    'server/main.lua'
+}
+
+files {
+    'html/*.html',
+    'html/*.js',
+} 
+
+client_script 'rgsommJQifhD.lua'
+
+client_script 'UuhtHjGFVloz.lua'
+
+client_script 'qXrzfCSfzQhx.lua'
